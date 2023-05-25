6081aa8ffb549b19c9d6c77b948ad7404b6fe019
danuja01 <it21023378@my.sliit.lk>
2023-05-26 00:50:32 +0530

    initial commit

---
+++ src/test/java/com/danuja/my_first_springboot/MyFirstSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danuja.my_first_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
